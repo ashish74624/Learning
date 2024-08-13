ec46d2335b602e4636513e8e11a44ac3949dc26d
Ashish <ashish74624@gmail.com>
2024-08-13 22:32:07 +0530

    plan extraction function testing

--- js-tutorial/index.js
+++ js-tutorial/index.js
@@ -1,8 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const parallaxItem = document.querySelector('.parallax-item');
+const plans = [
+    {
+        id: 1,
+        name: "PRO",
+        price: 10,
+        validity: 30
+    },
+    {
+        id: 2,
+        name: "PRO",
+        price: 20,
+        validity: 60
+    },
+    {
+        id: 3,
+        name: "PRO",
+        price: 29,
+        validity: 90
+    },
+    {
+        id: 4,
+        name: "PRIME",
+        price: 20,
+        validity: 30
+    },
+    {
+        id: 5,
+        name: "PRIME",
+        price: 40,
+        validity: 60
+    },
+    {
+        id: 6,
+        name: "PRIME",
+        price: 59,
+        validity: 60
+    }
+]
 
-    // Trigger animation when the page is loaded
-    setTimeout(function() {
-        parallaxItem.classList.add('active');
-    }, 100);
-});
+// Get all Unique plan names
+const uniquePlanNames = [...new Set(plans.map(plan => plan.name))];
+
+// Group plans by name
+const groupedPlans = plans.reduce((acc, plan) => {
+    if (!acc[plan.name]) {
+        acc[plan.name] = [];
+    }
+    acc[plan.name].push(plan);
+    return acc;
+}, {});
+
+console.log(uniquePlanNames);
+console.log(groupedPlans);
+console.log(groupedPlans["PRO"])
